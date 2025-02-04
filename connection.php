bc482d491f50fe98f5fcd4f9a86ac7736f01ec42
Adrianne Melody <melodyguarin06@gmail.com>
2025-02-04 20:29:17 +0800

    updated connection

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-//connection to database
+//connection to database ---- palagay localhost tnx
 $conn = mysqli_connect("localhost: ", "root", "", "gsuinventory");
 if($conn) {
 
@@ -7,11 +7,32 @@ if($conn) {
 
 }
 if(isset($_POST['submit'])) {
+//declaring variables
+	$username = $_POST['employee_username'];
+	$password = $_POST['employee_password'];
 
-	$user = $_POST['username'];
-	$password = $_POST['password'];
+// to check if the user exists
+$sql = "SELECT 'employee_id' FROM users WHERE username = 'employee_username'";
+$result = $conn->query($sql);
 
-if (empty($user) || empty($pass)) {
+// if the user exists
+if($result->num_rows > 0) {
+  // fetch user's data
+  $row = $result->fetch_assoc();
+
+  if ($row['password'] === "") {
+    echo "Login Successfull!";
+  } else {
+    echo "Invalid credentials, please try again.";
+  }
+} else {
+  // if no user match with the username
+  echo "Invalid credentials, please try again.";
+}
+}
+
+//close the connection
+$conn->close();
 
 
 ?>
